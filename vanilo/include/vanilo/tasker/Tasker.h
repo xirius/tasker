3091092b5eff25ad72966c2c44f93edf7e58aaee
alex <anoutchine@gmail.com>
2020-06-10 16:47:44 +0200

    Added Task class definition and few unit tests

--- vanilo/include/vanilo/tasker/Tasker.h
+++ vanilo/include/vanilo/tasker/Tasker.h
@@ -6,10 +6,11 @@
 
 #include <cassert>
 #include <future>
+#include <iostream>
 #include <memory>
+#include <optional>
 
-namespace vanilo {
-namespace tasker {
+namespace vanilo::tasker {
 
     class TaskExecutor;
 
@@ -18,82 +19,294 @@ namespace tasker {
 
     class Task
     {
+      public:
+        virtual ~Task() = default;
+
+        virtual void run() = 0;
+    };
+
+    /// TaskExecutor interface
+    /// ========================================================================
+
+    class TaskExecutor
+    {
+      public:
+        virtual ~TaskExecutor() = default;
+
+        virtual size_t count() const                    = 0;
+        virtual void submit(std::unique_ptr<Task> task) = 0;
+    };
+
+    /// QueuedTaskExecutor interface
+    /// ========================================================================
+
+    class QueuedTaskExecutor: public TaskExecutor
+    {
+      public:
+        static std::unique_ptr<QueuedTaskExecutor> create();
+
+        /**
+         * @param maxCount the maximum number of task to process.
+         * @return the number of tasks that still need to be processed.
+         */
+        virtual size_t process(size_t maxCount) = 0;
+    };
+
+    /**
+     * This exception occur when you're waiting for a result, then a cancellation is notified.
+     */
+    class CancellationException final: public std::exception
+    {
+      public:
+        const char* what() const noexcept override
+        {
+            return "A task was canceled.";
+        }
     };
 
     namespace details {
 
         /**
-         * Provides an optional facility to store a value or an exception in the promise-future channel.
-         * @tparam T result type
+         * Holds cancellation token and exception callback of the current task.
          */
-        template <typename T>
-        class VANILO_EXPORT OptionalPromise
+        class TaskContext
         {
           public:
-            ~OptionalPromise()
+            TaskContext() = default;
+
+            explicit TaskContext(CancellationToken token): _token{std::move(token)}
             {
-                if (_valid) {
-                    promise()->~promise();
+            }
+
+            bool isCanceled() const noexcept
+            {
+                return _token.isCanceled();
+            }
+
+            bool hasExceptionCallback() const noexcept
+            {
+                return (bool)_onException;
+            }
+
+            void onException(const std::exception& ex) const
+            {
+                if (_onException) {
+                    _onException(ex);
+                }
+                else {
+                    std::cerr << "An unhandled exception occurred during task execution. Message: " << ex.what() << std::endl;
                 }
             }
 
-            bool isValid() const noexcept
+            void setExceptionCallback(std::function<void(const std::exception&)> callback)
             {
-                return _valid;
-            }
-
-            template <typename R = T>
-            auto getFuture() -> typename std::enable_if<std::is_void<R>::value, std::future<R>>::type
-            {
-                assert(!_valid && "Future can only be taken once!");
-                _valid = true;
-                new (&_data) std::promise<R>{};
-                return promise()->get_future();
-            }
-
-            template <typename R = T>
-            auto getFuture() -> typename std::enable_if<!std::is_void<R>::value, std::future<R>>::type
-            {
-                assert(!_valid && "Future can only be taken once!");
-                _valid = true;
-                new (&_data) std::promise<R>{};
-                return promise()->get_future();
-            }
-
-            template <typename R = T, typename std::enable_if<std::is_void<R>::value, bool>::type = true>
-            void setValue()
-            {
-                assert(_valid && "Future must be taken first!");
-                promise()->set_value();
-            }
-
-            template <typename Q = T, typename std::enable_if<!std::is_void<Q>::value, bool>::type = true>
-            void setValue(Q&& value)
-            {
-                assert(_valid && "Future must be taken first!");
-                promise()->set_value(std::forward<T>(value));
-            }
-
-            void setException(std::exception_ptr exceptionPtr)
-            {
-                assert(_valid && "Future must be taken first!");
-                promise()->set_exception(exceptionPtr);
+                _onException = std::move(callback);
             }
 
           private:
-            using storage_t = typename std::aligned_storage<sizeof(std::promise<T>), alignof(std::promise<T>)>::type;
+            std::function<void(const std::exception&)> _onException;
+            CancellationToken _token;
+        };
 
-            std::promise<T>* promise()
+        /**
+         * Base class of a chainable task.
+         */
+        class LinkedTask: public Task
+        {
+          public:
+            explicit LinkedTask(TaskExecutor* executor): _executor{executor}
             {
-                return reinterpret_cast<std::promise<T>*>(&_data);
             }
 
-            storage_t _data{};
-            bool _valid{false};
+            void setContext(TaskContext context)
+            {
+                _context = std::move(context);
+            }
+
+            void setNext(std::unique_ptr<LinkedTask> task)
+            {
+                assert(task && "Task cannot be null!");
+                _next = std::move(task);
+            }
+
+          protected:
+            template <typename Callable, typename Result, typename Arg>
+            friend class BaseTask;
+
+            virtual void handleException() = 0;
+
+            LinkedTask* lastTask()
+            {
+                auto task = this;
+
+                while (task->_next) {
+                    task = task->_next.get();
+                }
+
+                return task;
+            }
+
+            inline void scheduleNext()
+            {
+                _next->setContext(std::move(_context));
+                _next->_executor->submit(std::move(_next));
+            }
+
+            TaskContext _context;
+            TaskExecutor* _executor;
+            std::unique_ptr<LinkedTask> _next;
         };
+
+        /**
+         * Argument dependent generic chainable task.
+         * @tparam Arg
+         */
+        template <typename Arg>
+        class ParametricTask: public LinkedTask
+        {
+          public:
+            using LinkedTask::LinkedTask;
+
+            void setArgument(Arg&& arg)
+            {
+                _param = std::move(arg);
+            }
+
+          protected:
+            Arg _param;
+        };
+
+        /**
+         * Argument dependent generic chainable task specialization in case the argument is void.
+         */
+        template <>
+        class ParametricTask<void>: public LinkedTask
+        {
+          public:
+            using LinkedTask::LinkedTask;
+        };
+
+        /**
+         * Generic task returning Result and taking Arg as a parameter.
+         * @tparam Callable The type of the task
+         * @tparam Result The result type returned by the task
+         * @tparam Arg The argument type of the task
+         */
+        template <typename Callable, typename Result, typename Arg>
+        class BaseTask: public ParametricTask<Arg>
+        {
+          public:
+            explicit BaseTask(TaskExecutor* executor, Callable&& task): ParametricTask<Arg>{executor}, _task{std::move(task)}
+            {
+            }
+
+            std::future<Result> getFuture()
+            {
+                _promise.emplace();
+                return _promise->get_future();
+            }
+
+          protected:
+            inline void dispatchException()
+            {
+                LinkedTask* task = this->lastTask();
+                task->setContext(std::move(this->_context));
+                task->handleException();
+            }
+
+            inline bool hasPromise() const noexcept
+            {
+                return _promise.has_value();
+            }
+
+            template <typename T>
+            inline void setPromiseValue(T&& value)
+            {
+                _promise->set_value(std::forward<T>(value));
+            }
+
+            inline Callable& task()
+            {
+                return _task;
+            }
+
+          private:
+            void handleException() override
+            {
+                try {
+                    if (this->_context.hasExceptionCallback()) {
+                        onFailure();
+                    }
+                    else if (_promise) { // If std::future was taken.
+                        _promise->set_exception(std::current_exception());
+                    }
+                }
+                catch (...) {
+                    // set_exception() or onFailure may throw too. Ignore it.
+                }
+            }
+
+            inline void onFailure() const
+            {
+                try {
+                    std::rethrow_exception(std::current_exception());
+                }
+                catch (const std::exception& ex) {
+                    this->_context.onException(ex);
+                }
+                catch (...) {
+                    this->_context.onException(std::runtime_error{"Unknown error occurred during execution of the task."});
+                }
+            }
+
+            std::optional<std::promise<Result>> _promise;
+            Callable _task;
+        };
+
+        /**
+         * Generic task specialization.
+         * @tparam Callable The type of the task
+         * @tparam Result The result type returned by the task
+         * @tparam Arg The argument type of the task
+         */
+        template <typename Callable, typename Result, typename Arg>
+        class Invocable: public BaseTask<Callable, Result, Arg>
+        {
+          public:
+            using BaseTask<Callable, Result, Arg>::BaseTask;
+
+            void run() override
+            {
+                try {
+                    if (this->_next) {
+                        // Case 1 : we have a next task to execute
+                        auto next = dynamic_cast<ParametricTask<Result>*>(this->_next.get());
+                        next->setArgument(executeTask());
+                        this->scheduleNext();
+                    }
+                    else if (this->hasPromise()) {
+                        // Case 2 : Future was taken
+                        this->setPromiseValue(executeTask());
+                    }
+                    else {
+                        // Case 3 : No result and no next task
+                        executeTask();
+                    }
+                }
+                catch (...) {
+                    this->dispatchException();
+                }
+            }
+
+          private:
+            inline Result executeTask()
+            {
+                return this->task()(std::move(this->_param));
+            }
+        };
+
     } // namespace details
 
-} // namespace tasker
-} // namespace vanilo
+} // namespace vanilo::tasker
 
 #endif // INC_CF1B33A15FDE47BDA967EACB24A90BED
--- vanilo/src/vanilo/tasker/Tasker.cpp
+++ vanilo/src/vanilo/tasker/Tasker.cpp
@@ -1,3 +1,75 @@
 #include <vanilo/tasker/Tasker.h>
 
+#include <queue>
+
 using namespace vanilo::tasker;
+
+/// DefaultQueuedTaskExecutor
+/// ============================================================================
+
+class DefaultQueuedTaskExecutor: public QueuedTaskExecutor
+{
+  public:
+    size_t count() const override
+    {
+        return _queueSize;
+    }
+
+    size_t process(size_t maxCount) override
+    {
+        size_t counter   = 0;
+        size_t queueSize = 0;
+
+        if (counter >= maxCount)
+            return _queueSize.load();
+
+        while (auto task = nextTask(queueSize)) {
+            try {
+                task->run();
+            }
+            catch (const std::exception& ex) {
+                std::cerr << "An exception occurred during task execution. Message: " << ex.what() << std::endl;
+            }
+
+            if (++counter >= maxCount)
+                break;
+        }
+
+        return queueSize;
+    }
+
+    void submit(std::unique_ptr<Task> task) override
+    {
+        auto guard = std::lock_guard(_mutex);
+        _queue.emplace(std::move(task));
+        ++_queueSize;
+    }
+
+  private:
+    inline std::unique_ptr<Task> nextTask(size_t& queueSize)
+    {
+        auto guard = std::lock_guard(_mutex);
+
+        if (_queue.empty())
+            return nullptr;
+
+        auto task = std::move(_queue.front());
+        _queue.pop();
+
+        queueSize = --_queueSize;
+        return task;
+    }
+
+  private:
+    std::queue<std::unique_ptr<Task>> _queue;
+    std::atomic<size_t> _queueSize{0};
+    mutable std::mutex _mutex;
+};
+
+/// QueuedTaskExecutor
+/// ============================================================================
+
+std::unique_ptr<QueuedTaskExecutor> QueuedTaskExecutor::create()
+{
+    return std::make_unique<DefaultQueuedTaskExecutor>();
+}
