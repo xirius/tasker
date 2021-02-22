c52d9a2fdc229c14870b4287a3d1c37c38f7bc25
alex <anoutchine@gmail.com>
2021-02-22 20:45:23 +0100

    Generalised function traits template

--- vanilo/include/vanilo/core/Traits.h
+++ vanilo/include/vanilo/core/Traits.h
@@ -4,44 +4,57 @@
 #include <tuple>
 
 namespace vanilo::core::traits {
+    namespace details {
+
+        template <typename Signature, typename = void>
+        struct FunctionTraitsBase;
+
+    } // namespace details
 
     /// Function traits
     /// ========================================================================
 
-    template <typename T, typename = void>
-    struct function_traits;
-
-    template <typename TReturn, typename... Args>
-    struct function_traits<TReturn (*)(Args...)>
+    template <typename Signature>
+    struct FunctionTraits: details::FunctionTraitsBase<Signature>
     {
-        static constexpr auto arity = sizeof...(Args);
-        using return_type           = TReturn;
-        using class_type            = void;
-        using args_type             = typename std::tuple<Args...>;
+        static constexpr bool IsMemberFnPtr = std::is_member_function_pointer<Signature>::value;
     };
 
-    template <typename TReturn, typename TClass, typename... Args>
-    struct function_traits<TReturn (TClass::*)(Args...)>
-    {
-        static constexpr auto arity = sizeof...(Args);
-        using return_type           = TReturn;
-        using class_type            = void;
-        using args_type             = typename std::tuple<Args...>;
+    namespace details {
+
+#define FUNCTION_TRAITS_TEMPLATE(CV, REF, L_VAL, R_VAL)             \
+    template <typename TReturn, typename TClass, typename... Args>  \
+    struct FunctionTraitsBase<TReturn (TClass::*)(Args...) CV REF>  \
+    {                                                               \
+        using ReturnType            = TReturn;                      \
+        using ClassType             = TClass;                       \
+        using ArgTypes              = typename std::tuple<Args...>; \
+        static constexpr auto Arity = sizeof...(Args);              \
     };
 
-    template <typename TReturn, typename TClass, typename... Args>
-    struct function_traits<TReturn (TClass::*)(Args...) const> // const
-    {
-        static constexpr auto arity = sizeof...(Args);
-        using return_type           = TReturn;
-        using class_type            = TClass;
-        using args_type             = typename std::tuple<Args...>;
-    };
+#define FUNCTION_TRAITS(REF, L_VAL, R_VAL)              \
+    FUNCTION_TRAITS_TEMPLATE(, REF, LVAL, RVAL)         \
+    FUNCTION_TRAITS_TEMPLATE(const, REF, LVAL, RVAL)    \
+    FUNCTION_TRAITS_TEMPLATE(volatile, REF, LVAL, RVAL) \
+    FUNCTION_TRAITS_TEMPLATE(const volatile, REF, LVAL, RVAL)
 
-    template <typename T>
-    struct function_traits<T, std::void_t<decltype(&T::operator())>>: public function_traits<decltype(&T::operator())>
-    {
-    };
+        FUNCTION_TRAITS(, true_type, true_type)
+        FUNCTION_TRAITS(&, true_type, false_type)
+        FUNCTION_TRAITS(&&, false_type, true_type)
+        FUNCTION_TRAITS(noexcept, true_type, true_type)
+        FUNCTION_TRAITS(&noexcept, true_type, false_type)
+        FUNCTION_TRAITS(&&noexcept, false_type, true_type)
+
+#undef FUNCTION_TRAITS
+#undef FUNCTION_TRAITS_TEMPLATE
+
+        template <typename Signature>
+        struct FunctionTraitsBase<Signature, std::void_t<decltype(&Signature::operator())>>
+            : public FunctionTraitsBase<decltype(&Signature::operator())>
+        {
+        };
+
+    } // namespace details
 } // namespace vanilo::core::traits
 
 #endif // INC_5CCBDE6C07F04D0090FDBF17F4FDADC7
