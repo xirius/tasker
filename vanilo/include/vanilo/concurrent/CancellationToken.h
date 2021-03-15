1193c770c328cd304c3d550150ee7785062716b0
alex <anoutchine@gmail.com>
2021-03-15 23:59:17 +0100

    Extended cancellation token

--- vanilo/include/vanilo/concurrent/CancellationToken.h
+++ vanilo/include/vanilo/concurrent/CancellationToken.h
@@ -2,6 +2,7 @@
 #include <vanilo/Export.h>
 
 #include <atomic>
+#include <functional>
 #include <memory>
 
 namespace vanilo::concurrent {
@@ -12,6 +13,8 @@ namespace vanilo::concurrent {
     class VANILO_EXPORT CancellationToken
     {
       public:
+        class RegistrationToken;
+
         CancellationToken();
 
         bool operator==(const CancellationToken& other) const noexcept;
@@ -19,10 +22,26 @@ namespace vanilo::concurrent {
 
         void cancel() noexcept;
         [[nodiscard]] bool isCanceled() const noexcept;
+        RegistrationToken registerAction(std::function<void()> callback);
 
       private:
         struct Impl;
         std::shared_ptr<Impl> _impl;
+    };
+
+    class VANILO_EXPORT CancellationToken::RegistrationToken
+    {
+        friend CancellationToken;
+
+      public:
+        RegistrationToken(RegistrationToken&& other) noexcept = default;
+        ~RegistrationToken();
+
+      private:
+        explicit RegistrationToken(CancellationToken& token);
+
+        struct Impl;
+        std::unique_ptr<Impl> _impl;
     };
 
 } // namespace vanilo::concurrent
--- vanilo/src/vanilo/concurrent/CancellationToken.cpp
+++ vanilo/src/vanilo/concurrent/CancellationToken.cpp
@@ -1,4 +1,8 @@
 #include <vanilo/concurrent/CancellationToken.h>
+#include <vanilo/core/Tracer.h>
+
+#include <map>
+#include <mutex>
 
 using namespace vanilo::concurrent;
 
@@ -7,7 +11,30 @@ using namespace vanilo::concurrent;
 
 struct CancellationToken::Impl
 {
-    std::atomic<bool> canceled;
+    void notify()
+    {
+        for (auto& pair : callbacks) {
+            try {
+                pair.second();
+            }
+            catch (const std::exception& ex) {
+                TRACE("An unhandled exception occurred during cancellation notification. Message: %s", ex.what());
+            }
+            catch (...) {
+                TRACE("An unhandled exception occurred during cancellation notification!");
+            }
+        }
+    }
+
+    void unregister(uintptr_t id)
+    {
+        std::lock_guard<std::mutex> lock{mutex};
+        callbacks.erase(id);
+    }
+
+    std::atomic<bool> canceled{};
+    std::map<uintptr_t, std::function<void()>> callbacks{};
+    std::mutex mutex{};
 };
 
 /// CancellationToken
@@ -30,9 +57,47 @@ bool CancellationToken::operator!=(const CancellationToken& other) const noexcep
 void CancellationToken::cancel() noexcept
 {
     _impl->canceled.store(true);
+    _impl->notify();
 }
 
 bool CancellationToken::isCanceled() const noexcept
 {
     return _impl->canceled.load();
 }
+
+CancellationToken::RegistrationToken CancellationToken::registerAction(std::function<void()> callback)
+{
+    std::lock_guard<std::mutex> lock{_impl->mutex};
+    RegistrationToken token{*this};
+    _impl->callbacks.emplace(reinterpret_cast<uintptr_t>(token._impl.get()), std::move(callback));
+    return token;
+}
+
+/// RegistrationToken::Impl
+/// ========================================================================
+
+struct CancellationToken::RegistrationToken::Impl
+{
+    explicit Impl(CancellationToken& token): object{token._impl}, id{reinterpret_cast<uintptr_t>(this)}
+    {
+    }
+
+    ~Impl()
+    {
+        if (auto token = object.lock()) {
+            token->unregister(id);
+        }
+    }
+
+    std::weak_ptr<CancellationToken::Impl> object;
+    uintptr_t id;
+};
+
+/// RegistrationToken
+/// ========================================================================
+
+CancellationToken::RegistrationToken::RegistrationToken(CancellationToken& token): _impl{std::make_unique<RegistrationToken::Impl>(token)}
+{
+}
+
+CancellationToken::RegistrationToken::~RegistrationToken() = default;
